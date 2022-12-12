ecda7cb54d35f394c8281e579be6169d16df3002
FirdaousseM <83703973+FirdaousseM@users.noreply.github.com>
2022-12-12 12:24:45 +0100

    Update ThreadServeur.java

--- Client_Serveur/src/serveur/ThreadServeur.java
+++ Client_Serveur/src/serveur/ThreadServeur.java
@@ -23,16 +23,13 @@ public class ThreadServeur extends Thread{
 			BufferedReader receptionMsg = new BufferedReader(new InputStreamReader(clientEmetteur.getInputStream()));
 			
 			envoiMsg = new PrintWriter(clientEmetteur.getOutputStream(), true);
-			//tant que le client est connecté
 			while(true){
-				//System.out.println("Client : " + msg);
 				String message = receptionMsg.readLine();
 				if (message.equals("\\quit")) {
 	                break;
 	            }
 				diffusionAuxClients(message);
 			}
-			//sortir de la boucle si le client a déconecté
 			System.out.println("Client déconnecté");
 		} catch (IOException e) {
 			e.printStackTrace();
